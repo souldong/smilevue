6db3000dcd9a4df5881a04a125bafdef7c770d9c
18234108360 <18234108360@163.com>
2019-12-02 23:01:44 +0800

    ss

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins:[
+    ['import',{
+      libraryName: 'vant',
+      libraryDirectry: 'es',
+      style: true
+    }, 'vant']
   ]
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'shoppingMall',
+    component: () => import(/* webpackChunkName: "about" */ '../views/shoppingMall/index.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/shoppingMall',
+    name: 'shoppingMall',
+    component: () => import(/* webpackChunkName: "about" */ '../views/shoppingMall/index.vue')
   }
 ]
 
